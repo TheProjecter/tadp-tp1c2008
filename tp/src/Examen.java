9ed4412d4ae1638d087087a044a3aafcf40d367d
tadp.techie <tadp.techie@5f0cb223-e949-0410-ab16-2dcb40bf5c27>
2008-04-20 04:41:27 +0000

    + Subido codigo de maxi para primer entrega (proyecto eclipse)

---
+++ tp/src/Examen.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Examen {
+
+	private Calendar fecha;
+	private Set<String> unidadesAbarcadas;
+	private Set<Pregunta> preguntas;
+	
+	
+	public Examen(Calendar fecha, Set<String> unidadesAbarcadas, Set<Pregunta> preguntas) 
+	{
+		this.fecha = fecha;
+		this.unidadesAbarcadas = unidadesAbarcadas;
+		this.preguntas = preguntas;
+	}
+
+
+	public Calendar getFecha() {
+		return fecha;
+	}
+
+
+	public Set<String> getUnidades() {
+		return unidadesAbarcadas;
+	}
+
+
+	public Set<Pregunta> getPreguntas() {
+		return preguntas;
+	}
+	
+	
+	
+}
---
+++ tp/src/Materia.java
@@ -0,0 +1,92 @@
+import java.util.*;
+public class Materia {
+
+	private Set<Pregunta> preguntas;
+	private List<Examen> examenes;
+	
+	public Materia()
+	{
+		
+		preguntas = new TreeSet<Pregunta>();
+		examenes = new ArrayList<Examen>();
+		
+		
+	}
+
+	public Set<Pregunta> getPreguntas() 
+	{
+		return preguntas;
+	}
+
+	public List<Examen> getExamenes() 
+	{
+		return examenes;
+	}
+	
+	public Examen generarExamen(Calendar fechaQueSeraTomado,Set<String> unidadesAbarcadas, int cantidadPreguntasTeoricas, int cantidadPreguntasPracticas)
+	{
+		int contadorPreguntasTeoricas = 0;
+		int contadorPreguntasPracticas = 0;
+		Set<Pregunta> preguntasParaElExamen = new HashSet<Pregunta>();
+		
+		
+		
+			for(Pregunta pregunta : preguntas)
+		
+			{
+				if(contadorPreguntasTeoricas < cantidadPreguntasTeoricas)			
+				{
+					if((unidadesAbarcadas.contains(pregunta.getUnidadTematica())) && (pregunta.getTipo()== Pregunta.TiposPregunta.TEORICO))
+				
+						{
+							System.out.println("Teorico");
+							preguntasParaElExamen.add(pregunta);
+							contadorPreguntasTeoricas++;
+						}
+				}
+			}
+		
+		
+			
+			for(Pregunta pregunta : preguntas)
+			{
+				if(contadorPreguntasPracticas < cantidadPreguntasPracticas)			
+				{				
+					if((unidadesAbarcadas.contains(pregunta.getUnidadTematica())) && (pregunta.getTipo()== Pregunta.TiposPregunta.PRACTICO))
+					{
+						System.out.println("Practico");
+						preguntasParaElExamen.add(pregunta);
+						contadorPreguntasPracticas++;
+					}
+				}
+			}
+			
+		
+		Examen examen = new Examen(fechaQueSeraTomado,unidadesAbarcadas,preguntasParaElExamen);
+		
+		
+		for(Pregunta p : examen.getPreguntas())
+		{
+			System.out.println(p.getPregunta());
+		}
+			
+		
+		
+		return examen;
+	}
+	
+	
+	public void recorreP()
+	{//Metodo Trucho para ver si me ordeno el TreeSet
+		
+		for(Pregunta pregunta : preguntas)
+		{
+			
+			System.out.println(pregunta.getPregunta());
+			
+		}
+		
+		
+	}
+	
+}
