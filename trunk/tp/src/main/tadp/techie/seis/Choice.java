386ddb23f97fb2d425cc53c66eb0ba489720383f
martin.scovotti <martin.scovotti@5f0cb223-e949-0410-ab16-2dcb40bf5c27>
2008-04-21 20:30:54 +0000

    PACHI MODIFIED

--- trunk/tp/src/main/tadp/techie/seis/Choice.java
+++ trunk/tp/src/main/tadp/techie/seis/Choice.java
@@ -1,12 +1,27 @@
 package tadp.techie.seis;
 
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Choice extends Pregunta{
+	
+	private List<String> opciones; //opciones para las respuestas
 
     public Choice(String unidadTematica, int complejidad, String pregunta,
-                     TiposPregunta tipo)
-    {
-        super(unidadTematica, complejidad, pregunta, tipo);
+            TiposPregunta tipo, List<String> opciones){
+    	super(unidadTematica, complejidad, pregunta, tipo);
+    	this.opciones = new ArrayList<String>();
+    	this.opciones = opciones;
     }
+
+	public List<String> getOpciones() {
+		return opciones;
+	}
+
+	public void setOpciones(List<String> opciones) {
+		this.opciones = opciones;
+	}
+    
+    
+    
 }
