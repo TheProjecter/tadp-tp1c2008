08bb85621bc70a952c889161af3220cd1b7cf95b
juanmartinporcel <juanmartinporcel@5f0cb223-e949-0410-ab16-2dcb40bf5c27>
2008-04-22 19:03:00 +0000

    Cree este proyecto (porque la verdad no supe como solucinar el lio de los classpath. ) que es el mismo que antes solo que agregue dos clases test una para poregunta... al pedo pero es para probar ..seguro vuela dsps. Y materiaTest que falta completar.

---
+++ TP.1.0/src/ar/utn/frba/tadp/techie/seis/exambase/ADesarrollar.java
@@ -0,0 +1,13 @@
+package ar.utn.frba.tadp.techie.seis.exambase;
+
+
+
+public class ADesarrollar extends Pregunta
+{
+    public ADesarrollar(String unidadTematica, int complejidad, String pregunta,
+                     TiposPregunta tipo)
+    {
+        super(unidadTematica, complejidad, pregunta, tipo);
+    }
+    
+}
