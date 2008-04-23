daabb76adb2e5cf7900c3f3cb32fd5525bae44a0
juanmartinporcel <juanmartinporcel@5f0cb223-e949-0410-ab16-2dcb40bf5c27>
2008-04-23 02:03:58 +0000

    addPregunta en Examen y Materia MateriaTest PreguntaTest ---> dsps volara si no sirve AllTests ---> no funca todavia porke el comparatorTest no esta implementado

---
+++ trunk/tp/src/test/tadp/techie/seis/test/AllTests.java
@@ -0,0 +1,14 @@
+package tadp.techie.seis.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	TP0TestUnitarioComparator.class,
+	PreguntaTest.class,
+	MateriaTest.class
+})
+public class AllTests {
+
+}
