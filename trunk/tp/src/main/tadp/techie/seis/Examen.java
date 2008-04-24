b3a1dfadf29100cd32333fa3751356e5cb5b47ac
jcfandino <jcfandino@5f0cb223-e949-0410-ab16-2dcb40bf5c27>
2008-04-24 03:52:03 +0000

    - Agregue un contructo para Examen  y que autoasigne las unidades tematicas en base a las materias que recibe

--- trunk/tp/src/main/tadp/techie/seis/Examen.java
+++ trunk/tp/src/main/tadp/techie/seis/Examen.java
@@ -18,15 +18,35 @@ public class Examen
      */
     public Examen(Calendar fecha, Set<String> unidadesAbarcadas, Set<Pregunta> preguntas) 
     {
-        this.fecha = fecha;
-        this.unidadesAbarcadas = unidadesAbarcadas;
-        this.preguntas = preguntas;
-     
-        //Incremento la cantidad de veces que se uso
-        for(Pregunta pregunta : preguntas)
-                pregunta.usoEnExamen();
+        this(fecha, preguntas);
+//        this.unidadesAbarcadas = unidadesAbarcadas;
+        for(String unidad : unidadesAbarcadas)
+        {
+            //Incluyo la unidad tematica
+            this.unidadesAbarcadas.add(unidad);
+        }
     }
-
+    /**
+     * Instancia un examen en una determinada Fecha que abarca unidades y tiene
+     * preguntas, las cuales se les avisa que se estan usando en un examen
+     * Las unidades abarcadas son las mismas que abarcan las materias
+     * @param fecha cuando se tomo/tomara
+     * @param preguntas preguntas a incluir
+     */
+    public Examen(Calendar fecha, Set<Pregunta> preguntas) 
+    {
+        this.fecha = fecha;
+        this.preguntas = preguntas;
+        this.unidadesAbarcadas = new HashSet<String>();
+        
+        for(Pregunta pregunta : preguntas)
+        {
+            //Incremento la cantidad de veces que se uso
+            pregunta.usoEnExamen();
+            //Incluyo la unidad tematica
+            this.unidadesAbarcadas.add(pregunta.getUnidadTematica());
+        }
+    }
 
     public Calendar getFecha()
     {
