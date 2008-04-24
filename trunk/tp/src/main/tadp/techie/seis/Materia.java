f004acdf8c95a7583988754e47eb95865cf3377b
jcfandino <jcfandino@5f0cb223-e949-0410-ab16-2dcb40bf5c27>
2008-04-24 03:00:43 +0000

    - Agregue para que incremente el contador de cuantas veces se uso la pregunta cuando se crean los examenes

--- trunk/tp/src/main/tadp/techie/seis/Materia.java
+++ trunk/tp/src/main/tadp/techie/seis/Materia.java
@@ -76,6 +76,10 @@ public class Materia
         preguntasParaElExamen.addAll(practicas);
         preguntasParaElExamen.addAll(teoricas);
 
+        //Incremento la cantidad de veces que se uso
+        for(Pregunta pregunta : preguntasParaElExamen)
+                pregunta.setCantidadDeVecesQueSeUso(pregunta.getCantidadDeVecesQueSeUso()+1);
+        
         //Instancio
         Examen examen = new Examen(fechaQueSeraTomado,unidadesAbarcadas,preguntasParaElExamen);
         //Me lo guardo
