3a633db3603720d397e8af0b4818c28beb91c518
juanmartinporcel <juanmartinporcel@5f0cb223-e949-0410-ab16-2dcb40bf5c27>
2008-04-24 18:58:59 +0000

    MateriaTest agregue un caso de test para ver que se usen todas las preguntas antes de repetir... pero debugeandolo vie que siempre me carga todas las preguntas... habria que revisar eso... hoy a la noche lo vemos.. (en clase pregunta) dsps incremantarUso cmabie el nombre nomas de usoEnExamen agregue el equals para examen fijense si les parece bien.

--- tp/src/main/tadp/techie/seis/Choice.java
+++ tp/src/main/tadp/techie/seis/Choice.java
@@ -6,7 +6,13 @@ import java.util.List;
 public class Choice extends Pregunta{
 	
 	private List<String> opciones; //opciones para las respuestas
-
+	/**
+	 * @param unidadTematica que unidad abarca?
+	 * @param complejidad nivel de complejidad?
+	 * @param pregunta que preguntas?
+	 * @param tipo tipo de pregunta? [TEORICO - PRACTICO - TEORICOPRACTICO]
+	 * @param opciones De una lista de opciones para responder.
+	 */
     public Choice(String unidadTematica, int complejidad, String pregunta,
             TiposPregunta tipo, List<String> opciones){
     	super(unidadTematica, complejidad, pregunta, tipo);
