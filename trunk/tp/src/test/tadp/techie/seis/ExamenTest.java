7fe298a0b08cade69a4f31dbda9d68cdf8ec7568
juanmartinporcel <juanmartinporcel@5f0cb223-e949-0410-ab16-2dcb40bf5c27>
2008-04-25 05:15:09 +0000

    agregue la clase ExamenTest y los posibles casos de test...faltan implementarlos =P .. mania sigo es muy tarde

---
+++ trunk/tp/src/test/tadp/techie/seis/ExamenTest.java
@@ -0,0 +1,44 @@
+package tadp.techie.seis;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ExamenTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testExamenCalendarSetOfStringSetOfPregunta() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testExamenCalendarSetOfPregunta() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAddPregunta() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testBorrarPreguntas() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testEqualsObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
