fd02fd5dfc90be6992993dd8f078409b3cddc8c1
martin.scovotti <martin.scovotti@5f0cb223-e949-0410-ab16-2dcb40bf5c27>
2008-04-25 12:49:04 +0000

    Agregue un comentario que explica cuando aparece la excepcion

--- trunk/tp/src/main/tadp/techie/seis/PreguntasInsuficientesException.java
+++ trunk/tp/src/main/tadp/techie/seis/PreguntasInsuficientesException.java
@@ -6,8 +6,9 @@
 package tadp.techie.seis;
 
 /**
- *
  * @author xuan
+ * La excepcion se presenta en el caso de que se quiera generar un nuevo examen
+ * con cierta cantidad de preguntas y que no esten disponible dicha cantidad
  */
 class PreguntasInsuficientesException extends Exception
 {
