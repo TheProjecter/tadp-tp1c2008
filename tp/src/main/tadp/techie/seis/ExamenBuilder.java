525c6ec4ad392c8be80b91e00c6f95e471541646
jcfandino <jcfandino@5f0cb223-e949-0410-ab16-2dcb40bf5c27>
2008-06-05 18:54:10 +0000

    un par de metodos que me olvide

--- tp/src/main/tadp/techie/seis/ExamenBuilder.java
+++ tp/src/main/tadp/techie/seis/ExamenBuilder.java
@@ -2,6 +2,7 @@ package tadp.techie.seis;
 
 import java.util.Calendar;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -13,6 +14,20 @@ public class ExamenBuilder
 	
 	private Materia materia;
 	private Set<String> unidadesAbarcadas;
+	
+	public ExamenBuilder()
+	{
+		this.setUnidadesAbarcadas(new HashSet<String>());
+	}
+	
+	public ExamenBuilder(Materia materia, Collection<String> unidadesAbarcadas)
+	{
+		this();
+		this.setMateria(materia);
+		this.getUnidadesAbarcadas().addAll(unidadesAbarcadas);
+	}
+	
+	
 	
 	HashMap<PrototipoItem<ItemExamen>, Integer> mapaPrototipos = new HashMap<PrototipoItem<ItemExamen>, Integer>();
 	
@@ -29,7 +44,7 @@ public class ExamenBuilder
 		
 		for(PrototipoItem<ItemExamen> proto : mapaPrototipos.keySet())
 		{
-			itemsTotales.addAll(obtenerItems(materia.getItems(), mapaPrototipos.get(proto), proto));
+			itemsTotales.addAll(obtenerItems(getMateria().getItems(), mapaPrototipos.get(proto), proto));
 		}
 		
 		return null;
@@ -50,14 +65,14 @@ public class ExamenBuilder
 			//Lo tengo que agregar si es de una unidad tematica que quiero y ademas se parece al prototipo
 			//Si el campo del prototipo por el que comparo es null o 0 no lo tengo que comprar entonces lo tomo
 			//como verdadero
-			if(		unidadesAbarcadas.contains(item.getUnidadTematica())
+			if(		getUnidadesAbarcadas().contains(item.getUnidadTematica())
 				&&	proto.itemSeParece(item)
 				)
 				retval.add(item);
 		}
 	
 		if(retval.size() != cantidad)
-			throw new PreguntasInsuficientesException("No hay suficientes items de tipo "+ proto.getTipo() +" en la materia "+materia.getNombre());
+			throw new PreguntasInsuficientesException("No hay suficientes items de tipo "+ proto.getTipo() +" en la materia "+getMateria().getNombre());
 	
 		return retval;
 	}
@@ -149,6 +164,41 @@ public class ExamenBuilder
 		if(misEjercicios.size() != cantidadDeEjercicios)
 			throw new PreguntasInsuficientesException("No hay suficientes preguntas de tipo "+ tipoEjercicio +" en la materia "+materia.getNombre());      
 		return misEjercicios;
+	}
+
+	public void setMateria(Materia materia)
+	{
+		this.materia = materia;
+	}
+
+	public Materia getMateria()
+	{
+		return materia;
+	}
+
+	private void setUnidadesAbarcadas(Set<String> unidadesAbarcadas)
+	{
+		this.unidadesAbarcadas = unidadesAbarcadas;
+	}
+
+	private Set<String> getUnidadesAbarcadas()
+	{
+		return unidadesAbarcadas;
+	}
+	
+	public void addUnidadAbarcada(String unidad)
+	{
+		unidadesAbarcadas.add(unidad);
+	}
+	
+	public void clearUnidadesAbarcadas()
+	{
+		unidadesAbarcadas.clear();
+	}
+	
+	public void addAllUnidadesAbarcadas(Collection<String> unidades)
+	{
+		unidadesAbarcadas.addAll(unidades);		
 	}
 
 }
