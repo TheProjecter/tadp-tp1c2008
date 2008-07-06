bbf62c987c1de92eabf2ac1cd4ce5f0cc1a25ecb
jcfandino <jcfandino@5f0cb223-e949-0410-ab16-2dcb40bf5c27>
2008-07-06 19:01:09 +0000

    tag de la 2da entrega

---
+++ tags/entrega2/src/main/tadp/techie/seis/Ejercicio.java
@@ -0,0 +1,35 @@
+package tadp.techie.seis;
+
+import java.util.Set;
+
+
+public class Ejercicio extends ItemExamen{
+	
+
+	/**
+	 * Ejercicio.<e>
+	 * 
+	 * Un ejercicio (a diferencia de una pregunta) no admite multiple choice.-
+	 * 
+	 * @param unidadTematica   	que unidad tematica?
+	 * @param complejidad		valor de complejidad?
+	 * @param ejercicio			que ejercicio?
+	 * @param tipo				tipo de ejercicio? [TEORICO - PRACTICO - TEORICOPRACTICO]
+	 */
+    public Ejercicio(String unidadTematica, int complejidad, String enunciado,
+                     TiposItem tipo) {
+    
+    	super(unidadTematica, complejidad, enunciado, tipo);
+                   
+
+    }
+    public  void addTo(ItemAddable materia){
+    	
+    	materia.addEjercicio(this);
+    }
+	@Override
+	public Set<? extends ItemExamen> getItemsFrom(Materia materia) {
+		return materia.getEjercicios();
+	}
+}
+
