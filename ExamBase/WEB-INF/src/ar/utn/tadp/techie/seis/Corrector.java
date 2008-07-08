691fc294e5a317c3e09390c604fce6f0add23032
jcfandino <jcfandino@5f0cb223-e949-0410-ab16-2dcb40bf5c27>
2008-07-08 04:51:33 +0000

    cambie los fuentes de lugar para que esten todos juntitos y ahora felices

--- ExamBase/WEB-INF/src/ar/utn/tadp/techie/seis/Corrector.java
+++
@@ -1,9 +0,0 @@
-package ar.utn.tadp.techie.seis;
-
-public interface Corrector {
-
-	public ExamenCorregido.CalificacionPregunta getNotaPregunta(ItemExamen preg);
-	
-	public int getNotaFinal();
-
-}
