cb16f7d1ac2036e5abd1b48d8c331853dd091cfb
tadp.techie <tadp.techie@5f0cb223-e949-0410-ab16-2dcb40bf5c27>
2008-07-15 23:17:34 +0000

    mostrar preguntas en Home.html en forma dinamica.

--- ExamBase/WEB-INF/src/main/ar/utn/tadp/techie/seis/web/Home.java
+++ ExamBase/WEB-INF/src/main/ar/utn/tadp/techie/seis/web/Home.java
@@ -3,6 +3,9 @@ package ar.utn.tadp.techie.seis.web;
 
 //import org.apache.tapestry.annotations.InitialValue;
 //import org.apache.tapestry.annotations.InjectState;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.tapestry.form.IPropertySelectionModel;
 import org.apache.tapestry.form.StringPropertySelectionModel;
 import org.apache.tapestry.html.BasePage;
@@ -10,9 +13,17 @@ import org.apache.tapestry.IPage;
 import org.apache.tapestry.IRequestCycle;
 import org.apache.tapestry.annotations.*;
 
+import ar.utn.tadp.techie.seis.ADesarrollar;
+import ar.utn.tadp.techie.seis.ItemExamen;
+import ar.utn.tadp.techie.seis.Pregunta;
+
 
 public abstract class Home extends BasePage {
 	 
+	
+	private List<ItemExamen> items;
+	
+	
 	@InjectPage("ABMUnidades")
 	abstract public ABMUnidades getUnidadesPage() ;
 	
@@ -55,6 +66,25 @@ public abstract class Home extends BasePage {
 	{
 		GenerarExamen examenPage = getGenerarExamenPage();
 		return examenPage;
+		
+	}
+	
+	//TODO --este metodo deberia devolver las preguntas de la materia seleccionada
+	public List<ItemExamen> getItems() 
+	{
+		items = new ArrayList<ItemExamen>();
+		 
+		Pregunta pregunta = new ADesarrollar("Patrones", 75, "Por que necesitamos a los patrones en las estancias?", ItemExamen.TiposItem.TEORICO);   
+		items.add(pregunta);
+
+		pregunta = new ADesarrollar("Estructurado", 10, "Alguien usa estructurado hoy en Dia?", ItemExamen.TiposItem.TEORICO);  
+		items.add(pregunta);
+
+		pregunta = new ADesarrollar("Estructurado", 40, "Cuantos modos de Cohesion Existe?", ItemExamen.TiposItem.TEORICO);  
+		items.add(pregunta);
+		 
+		 
+		return items;
 		
 	}
 }
