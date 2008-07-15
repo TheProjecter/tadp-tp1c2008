77f87b90a11fbf61e1bfec51de8667e68d0dbcb5
tadp.techie <tadp.techie@5f0cb223-e949-0410-ab16-2dcb40bf5c27>
2008-07-15 19:59:26 +0000

    cambios en generar examen y detalles. JM.

--- ExamBase/WEB-INF/src/main/ar/utn/tadp/techie/seis/web/GenerarExamen.java
+++ ExamBase/WEB-INF/src/main/ar/utn/tadp/techie/seis/web/GenerarExamen.java
@@ -1,10 +1,25 @@
 package ar.utn.tadp.techie.seis.web;
 
+import org.apache.tapestry.annotations.InitialValue;
+import org.apache.tapestry.form.IPropertySelectionModel;
+import org.apache.tapestry.form.StringPropertySelectionModel;
 import org.apache.tapestry.html.BasePage;
 import org.apache.tapestry.record.PropertyChangeObserver;
 
-public class GenerarExamen extends BasePage {
+public abstract class GenerarExamen extends BasePage {
 
+	@InitialValue("literal:3")
+	public abstract String getCantidadPreguntasTeoricas();
+	@InitialValue("literal:3")
+	public abstract String getCantidadPreguntasPracticas();
+	@InitialValue("literal:3")
+	public abstract String getCantidadEjerciciosTeoricos();
+	@InitialValue("literal:1")
+	public abstract String getCantidadEjerciciosPracticos();
+	@InitialValue("literal:1")
+	public abstract String getCantidadUnidades();
+
+	
 	@Override
 	public String getClientId() {
 		// TODO Auto-generated method stub
@@ -22,5 +37,10 @@ public class GenerarExamen extends BasePage {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
+	public IPropertySelectionModel getUnidadesModel() {
+		// para prueba ...las materias van a venir del resultado de un query a la base
+		 String[] unidades = {"Unidad 1", "Unidad 2", "Unidad 3"}; 
+        return new StringPropertySelectionModel(unidades);
+    }
 }
