4264a44b1cacdd8b5ebc24c9bf3a80f2943f0b63
jcfandino <jcfandino@5f0cb223-e949-0410-ab16-2dcb40bf5c27>
2008-07-16 03:10:20 +0000

    Agregue la interfaz DAO para Materia con una clase stub para SQL Server y un Singleton para el Pool de materias, tambien un stub

---
+++ ExamBase/WEB-INF/src/main/ar/utn/tadp/techie/seis/persistance/MateriaDAO.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ar.utn.tadp.techie.seis.persistance;
+
+import java.util.Set;
+
+/**
+ *
+ * @author xuan
+ */
+public interface MateriaDAO
+{
+
+    Set<String> listarMaterias();
+    
+    
+}
---
+++ ExamBase/WEB-INF/src/main/ar/utn/tadp/techie/seis/pools/MateriasPool.java
@@ -0,0 +1,63 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ar.utn.tadp.techie.seis.pools;
+
+import ar.utn.tadp.techie.seis.Materia;
+import ar.utn.tadp.techie.seis.persistance.MateriaDAO;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author xuan
+ */
+public class MateriasPool
+{
+
+    private static MateriasPool instance;
+    
+    private Map<Materia,Boolean> mapaMaterias;
+    private int size;
+    private MateriaDAO dao;
+    
+    protected MateriasPool()
+    {
+        mapaMaterias = new HashMap<Materia,Boolean>();
+        size = 3; //TODO Hardcodeado!, sacarlo de configuracion
+    }
+    
+    public static MateriasPool getInstance()
+    {
+        if(instance == null)
+            instance = new MateriasPool();
+        return instance;
+    }
+    
+    /**
+     * Devuelve la instancia de la materia, si no esta en el pool
+     * la busca en la db
+     * @param nombre el nombre de una materia
+     * @return la materia con ese nombre
+     */
+    public Materia getMateria(String nombre) {
+        return null;
+    }
+    /**
+     * Avisa que la materia se modifico y antes de sacarla del pool
+     * hay que actualizar en la db. Sino se avisa cuando el pool elimine
+     * la materia va a pensar que nadie la cambio y se pierden los cambios
+     * @param mat la materia
+     */
+    public void materiaModified(Materia mat) {
+        
+    }
+    /**
+     * Fuerza que se guarden todos los cambios en la db
+     */
+    public void forceWrite() {
+        
+    }
+}
