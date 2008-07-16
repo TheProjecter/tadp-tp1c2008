a0427c42c92ef694ff78dc68d2c2d4b59730f93f
jcfandino <jcfandino@5f0cb223-e949-0410-ab16-2dcb40bf5c27>
2008-07-16 03:10:20 +0000

    Agregue la interfaz DAO para Materia con una clase stub para SQL Server y un Singleton para el Pool de materias, tambien un stub

---
+++ branches/tp2/ExamBase/WEB-INF/src/main/ar/utn/tadp/techie/seis/persistance/SQLServerMateriaDAO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ar.utn.tadp.techie.seis.persistance;
+
+import java.util.Set;
+
+/**
+ *
+ * @author xuan
+ */
+public class SQLServerMateriaDAO implements MateriaDAO
+{
+
+    public Set<String> listarMaterias()
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
